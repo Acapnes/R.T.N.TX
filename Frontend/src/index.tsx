2edb763a4d6428ef6c727f70da4a7203989e0473
Alper <61701011+Acapnes@users.noreply.github.com>
2022-04-09 21:12:50 +0300

    Router update

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import HomePage from './Home';
 import About from './About';
+import { SignUp } from './Auth/SignUp';
+import { Login } from './Auth/Login';
 
 import {
   BrowserRouter,
@@ -11,12 +13,15 @@ import {
   Route,
 } from "react-router-dom";
 
+
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}/>
-      <Route path="/home" element={<HomePage />}/>
-      <Route path="/About" element={<About />}/>
+      <Route path="/home" element={<HomePage/>}/>
+      <Route path="/About" element={<About/>}/>
+      <Route path="/Login" element={<Login/>}/>
+      <Route path="/Signup" element={<SignUp />}/>
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
