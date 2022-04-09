d546d80c497aa8d39f707ce6f6250a0d823fe088
Alper <61701011+Acapnes@users.noreply.github.com>
2022-04-09 18:57:32 +0300

    Login and Register pages design

---
+++ Frontend/src/Auth/SignUp.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const SignUp: React.FC = () => {
+
+    const [email, setEmail] = useState("");
+    const [emailErrored, setEmailErrored] = useState(false);
+
+
+    const [password, setPassword] = useState("");
+    const [passwordErrored, setPasswordErrored] = useState(false);
+
+    const handleSignUp = () => {
+        if (!email) {
+            setEmailErrored(true);
+        } else {
+            setEmailErrored(false);
+        }
+        if (!password) {
+            setPasswordErrored(true);
+        } else {
+            setPasswordErrored(false);
+        }
+    }
+
+    return (
+        <div className="h-screen w-screen bg-slate-500 flex justify-center items-center flex-col">
+            <span className="py-4 text-2xl text-white">Let's Sign up!</span>
+            <input
+                placeholder="E-mail"
+                className="mb-4 px-8 py-4 text-md outline-none rounded-2xl"
+                type="text"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+            />
+            <input
+                placeholder="Password"
+                className="mb-4 px-8 py-4 text-md outline-none rounded-2xl"
+                type="text"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+            />
+            <button onClick={handleSignUp} className=" m-4 px-8 py-4 rounded-2xl bg-purple-500 text-white">
+                Login!
+            </button>
+        </div>
+    )
+}
+
+export { SignUp };
